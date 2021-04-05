4d2b45cf8c2a723da5d4df5e079d5d802f0e8170
Matt Eckhardt <meckhardt14@yahoo.com>
2021-04-04 21:25:22 -0400

    Week 10 Homework added

---
+++ Week10/class/logs.ps1
@@ -0,0 +1,95 @@
+﻿# Storyline: View the event logs, check for a valid log, and print the results
+
+
+function select_log() {
+
+    # List all event logs
+    $theLogs = Get-Eventlog -list | select Log
+    $theLogs | Out-Host
+
+    # Initialize the array to store logs
+    $arrLog = @()
+
+    foreach ($tempLog in $theLogs) {
+        
+        # Add each log to array
+        # NOTE: These are stored in the array as a hashtable in the formate:
+        # @{Log=LOGNAME}
+        $arrLog += $tempLog 
+
+
+    }
+    #TEST array
+    #$arrLog[0]
+
+    #Prompt user to view a log or quit
+    $readLog = read-host -Prompt "Please enter a log from the list above or q to quit the program"
+
+    #check if the user wants to quit.
+    if ($readLog -match "^[qQ]$") {
+        #stop the program
+        break
+
+    }
+
+    log_check -logToSearch $readLog
+
+
+} #ends the select_log()
+
+
+
+
+
+function log_check() {
+
+
+    # String the user types in within the select_log function
+    Param([string]$logToSearch)
+
+    # Format the user input
+    # Example: @{Log=Security}
+    $theLog = "^@{Log=" + $logToSearch + "}$"
+
+    # Search the array for the exact hashtable string
+    if ($arrLog -match $theLog){
+    
+    write-host -BackgroundColor Green -ForegroundColor White "Please wait, it may take a few moments..."
+    sleep 2
+
+    # Call view_log to see the log
+    view_log -logToSearch $logToSearch
+
+    } else {
+
+        write-host -BackgroundColor Red -ForegroundColor White "The log specified doesn't exist."
+        sleep 2
+
+        select_log
+
+    }
+
+
+
+} #ends of log_check()
+
+
+
+
+
+function view_log() {
+    cls
+
+
+    # Get the logs
+    Get-EventLog -Log $logToSearch -Newest 10 -After "1/18/2020"
+
+    #Go back to select log
+    select_log
+
+
+} # ends view_log()
+
+
+# Run select_log
+select_log
---
+++ Week10/homework/getServiceLog.ps1
@@ -0,0 +1,50 @@
+﻿# Storyline: View the services, and either all of them, the running ones, or the stopped ones.
+# Liberally copied from the get event log script in the classwork
+
+function ServiceClient() { 
+    # List all services
+    Get-Service | select Name | Out-Host
+
+    #Prompt user to view services or quit or quit
+    $servicePrompt = read-host -Prompt "What services do you want to view, All, Running or Stopped? or q to quit the program"
+
+    # Store possible values
+    $validResponse =@('All', 'Stopped', 'Running')
+
+        
+    #check if the user wants to quit.
+    if ($servicePrompt -match "^[qQ]$") {
+        #stop the program
+        break
+
+    }
+
+
+    if ($validResponse -match $servicePrompt) {
+        
+        if ($servicePrompt -eq 'All') {
+
+            Get-Service | select Name, Status | Out-Host
+
+
+        } else {
+
+            Get-Service | where {$_.Status -eq $servicePrompt} | select Name, Status | Out-Host
+        }
+
+
+    } else {
+        ServiceClient
+
+    }
+
+
+
+  
+
+
+
+}
+
+
+ServiceClient
